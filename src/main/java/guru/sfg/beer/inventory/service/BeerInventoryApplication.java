4515c63e6ce3350e52ee7a1ebd3400fc0cc614d7
petar-georgiev <georgiev.peter1@gmail.com>
2022-12-11 11:27:23 +0200

    implement New Inventory Listener

--- src/main/java/guru/sfg/beer/inventory/service/BeerInventoryApplication.java
+++ src/main/java/guru/sfg/beer/inventory/service/BeerInventoryApplication.java
@@ -2,13 +2,11 @@ package guru.sfg.beer.inventory.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jms.artemis.ArtemisAutoConfiguration;
 
-@SpringBootApplication(exclude = ArtemisAutoConfiguration.class)
+@SpringBootApplication
 public class BeerInventoryApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BeerInventoryApplication.class, args);
     }
-
 }
